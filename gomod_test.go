38e48646c71ced63e3d7f87960bafdc2c5b14b8b
Flc゛ <four_leaf_clover@foxmail.com>
2024-12-14 23:41:31 +0800

    feat(gomod): Add test for go.mod version check (#423)
    * feat(gomod_test): Add test for go.mod version check
    
    * feat(gomod_test): Add test for go.mod version check

---
+++ gomod_test.go
@@ -0,0 +1,21 @@
+package components
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGoModVersion(t *testing.T) {
+	bytes, err := os.ReadFile("go.mod")
+	require.NoError(t, err)
+	content := string(bytes)
+
+	assert.NotContains(t, content, "toolchain")
+
+	contents := strings.Split(content, "\n")
+	assert.Subset(t, contents, []string{"go 1.22.0"})
+}

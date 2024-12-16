efa8f2e34beb53a6a91d3c33afe7b01d072ec49e
Flc゛ <four_leaf_clover@foxmail.com>
2024-12-16 23:13:06 +0800

    remove(crontab): remove distributednooverlapping/tracing middleware (#456)

--- crontab/example_test.go
+++ crontab/example_test.go
@@ -4,44 +4,20 @@ import (
 	"context"
 
 	"github.com/flc1125/go-cron/v4"
-	"github.com/go-kratos/kratos/v2"
-	"github.com/redis/go-redis/v9"
-
 	"github.com/go-kratos-ecosystem/components/v2/crontab"
-	"github.com/go-kratos-ecosystem/components/v2/crontab/middleware/distributednooverlapping"
-	"github.com/go-kratos-ecosystem/components/v2/crontab/middleware/distributednooverlapping/redismutex"
+	"github.com/go-kratos/kratos/v2"
 )
 
-type testJob struct {
-	distributednooverlapping.DefaultTTLJobWithMutex // optional, if you don't need to implement GetMutexTTL
-}
-
-var _ distributednooverlapping.JobWithMutex = (*testJob)(nil)
-
-func (m *testJob) Run(context.Context) error {
-	// do something
-	return nil
-}
-
-func (m *testJob) GetMutexKey() string {
-	return "test"
-}
-
 func Example() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-
 	c := cron.New(
 		cron.WithSeconds(),
-		cron.WithMiddleware(
-			distributednooverlapping.New(
-				redismutex.New(rdb, redismutex.WithPrefix("cron:mutex")),
-			),
-		),
+		cron.WithMiddleware( /*...*/ ),
 	)
 
-	_, _ = c.AddJob("* * * * * *", &testJob{})
+	_, _ = c.AddFunc("* * * * * *", func(context.Context) error {
+		// do something
+		return nil
+	})
 
 	// kratos app start
 	app := kratos.New(
